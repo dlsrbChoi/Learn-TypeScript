71974ac918ed123892c777ed1d6a634deca1a7c8
Ingyu Choi <awdzx456@naver.com>
2023-01-12 14:34:24 +0900

    learn-ts

---
+++ basic-typescript/why-ts/sample.js
@@ -0,0 +1,16 @@
+// function sum(a, b) {
+//   return a + b;
+// }
+// sum(10, '20')
+
+// @ts-check
+
+/**
+ *
+ * @param {number} a 첫 번째 숫자
+ * @param {number} b 두 번째 숫자
+ */
+function sum(a, b) {
+  return a + b;
+}
+sum(10, 20);
