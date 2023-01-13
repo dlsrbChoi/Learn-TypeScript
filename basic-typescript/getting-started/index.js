40a98d5e701fc313388b77360b3c737ce8e2271a
Ingyu Choi <awdzx456@naver.com>
2023-01-13 15:07:58 +0900

    learn-ts

--- basic-typescript/getting-started/index.js
+++
@@ -1,5 +0,0 @@
-// npm i typescript -g
-function sum(a, b) {
-    return a + b;
-}
-sum(10, 20);
--- basic-typescript/getting-started/index.ts
+++
@@ -1,9 +0,0 @@
-// 타입스크립트 파일을 자바스크립트로 변환하는 방법(컴파일)
-// npm i typescript -g
-// tsc index.ts
-
-function sum(a: number, b: number): number {
-    return a + b;
-}
-
-sum(10, 20);
